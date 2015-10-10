c199b19d1bb9de4d793533c692a74995952442c4
kowsalyajoshi <kowsalyas23@gmail.com>
2015-10-10 10:43:38 +0530

    created Mail.Php

---
+++ mail.php
@@ -0,0 +1,14 @@
+<?php 
+
+$name=$_POST['name'];
+$email=$_POST['email'];
+$message=$_POST['message'];
+
+$emailBody =   "Name: ".$name."\nEmail: ".$email."\nPhone: ".$phone."\n\nMessage: \n\n".$message."\n";  
+
+$headers = 'From: support@ctrlthreads.co.in' . "\r\n" .
+    'Reply-To: ' support@ctrlthreads.co.in. "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+mail('support@ctrlthreads.co.in', 'Contact - Us Request', $emailBody, $headers); 
+?>
